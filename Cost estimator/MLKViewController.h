7a551b004d85bf841287484a87ac057506a8bdd4
Michael Kosmicki <Kozi098@gmail.com>
2014-05-28 18:45:24 -0400

    step 1

--- Cost estimator/MLKViewController.h
+++ Cost estimator/MLKViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface MLKViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *roomLarge;
+@property (weak, nonatomic) IBOutlet UITextField *roomMedium;
+@property (weak, nonatomic) IBOutlet UITextField *roomSmall;
+@property (weak, nonatomic) IBOutlet UITextField *employees;
+@property (weak, nonatomic) IBOutlet UITextField *distence;
+@property (weak, nonatomic) IBOutlet UITextField *daysNeeded;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+- (IBAction)estimatePrice:(id)sender;
+
 
 @end
--- Cost estimator/MLKViewController.m
+++ Cost estimator/MLKViewController.m
@@ -13,10 +13,12 @@
 @end
 
 @implementation MLKViewController
-
+@synthesize roomLarge, roomSmall, roomMedium, employees, distence, totalLabel, daysNeeded;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeNumberPad)];
+    [self.view addGestureRecognizer:tap];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +28,31 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)estimatePrice:(id)sender {
+    
+    
+    int rooms = 0;
+    int workers = 0;
+    int dist = 0;
+    int days = 0;
+    int total = 0;
+    
+    rooms = ([roomLarge.text intValue ]*2) + ([roomMedium.text intValue]*1.5) + [roomSmall.text intValue];
+    workers = [employees.text intValue ];
+    dist = [distence.text intValue];
+    days = [daysNeeded.text intValue];
+    total = (rooms + ((15*workers*8)*days) + (((dist*2*days)/18)*3.50));
+    
+    totalLabel.text = [NSString stringWithFormat:@"%d to meet Baseline price",total];
+    
+}
+
+- (void)removeNumberPad{
+    [roomLarge resignFirstResponder];
+    [roomSmall resignFirstResponder];
+    [roomMedium resignFirstResponder];
+    [employees resignFirstResponder];
+    [distence resignFirstResponder];
+    [daysNeeded resignFirstResponder];
+}
 @end
